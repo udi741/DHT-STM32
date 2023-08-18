a9ee6e33814a498db747c1df82245ae3416b0c1d
udi741 <37002800+udi741@users.noreply.github.com>
2023-08-18 12:33:54 +0300

    accessing ODR instead using HAL, added option to not use FreeRTOS
    removed some debugging variables that left from testing phase

--- dht-stm32.h
+++ dht-stm32.h
@@ -5,15 +5,21 @@
  extern "C" {
 #endif
 
+#define USINGFREERTOS 1
+
 #include "main.h"
-#include "cmsis_os.h"
 #include <stdbool.h>
+#if (USINGFREERTOS == 1)
+	#include "cmsis_os.h"
+	#define DHTDelay(t)	osDelay(t)
+#else
+	#define DHTDelay(t)	HAL_Delay(t)
+#endif
 
 typedef struct
 {
 	float temperature;
 	float humidity;
-	float smoothTemp;
 }dhtData_t;
 
 typedef struct
@@ -23,13 +29,11 @@ typedef struct
 	TIM_HandleTypeDef* timer;
 	uint16_t lastCnt;
 	uint8_t binaryData[2*2+40*2];
-	uint16_t msgInterruptCnt;
-	uint16_t size;
-	
+	uint16_t msgInterruptCnt;	
 	dhtData_t data;
 }dht_t;
 
-void DHTinit(dht_t* dht, GPIO_TypeDef* gpio, uint16_t gpio_pin, TIM_HandleTypeDef* timer, uint16_t boardMaxClockFreqMHz);
+void DHTinit(dht_t* dht, GPIO_TypeDef* gpio, uint16_t gpio_pin, TIM_HandleTypeDef* timer, uint16_t timerClockFreqMHz);
 void DHTRequestData(dht_t* dht);
 void DHTConfigureToOutput(dht_t* dht);
 void DHTConfigureToInput(dht_t* dht);
@@ -38,6 +42,7 @@ void DHTSetTimerCnt(dht_t* dht, uint32_t timeToSet);
 void DHTInterruptCallback(dht_t* dht);
 bool DHTReadData(dht_t* dht, uint8_t* data);
 dhtData_t getDHTMeasurement(dht_t* dht);
+int32_t measureDeltaUs(dht_t* dht);
 
 #ifdef __cplusplus
 }
